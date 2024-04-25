87091deb655647e452472d1142f2ec8979231588
Cristiano José da Silva <149792686+cristianojs02@users.noreply.github.com>
2024-04-25 14:03:56 -0300

    Setup logging level. Change log points.

--- iris_deployer.py
+++ iris_deployer.py
@@ -13,7 +13,9 @@ class IrisDeployer(object):
     def __init__(self, host: str, port: int, namespace_iris: str, https: int,
                 api_base_url: str, iris_usr: str, iris_pwd: str, api_version: str,
                 compilation_flags: str, source_path: str) -> None:
-
+        
+        logging.basicConfig(level = logging.INFO)
+        
         self.__host = host
         self.__port = port
         self.__namespace_iris = namespace_iris
@@ -22,63 +24,54 @@ class IrisDeployer(object):
         self.__api_version = api_version
         self.__compilation_flags = compilation_flags
         self.__source_path = source_path
+        self.__has_error: bool = False
+        
         protocol: str = 'http' if self.__https == 0 else 'https'
 
         self.__PUT_DOC_URL: str = f'{protocol}://{self.__host}:{self.__port}'
-        self.__PUT_DOC_URL += f'/api/atelier/{self.__api_version}/{self.__namespace_iris}/doc/'
+        self.__PUT_DOC_URL += f'{self.__api_base_url}{self.__api_version}/{self.__namespace_iris}/doc/'
 
-        self.__DELETE_DOCS_URL: str = f'{protocol}://{self.__host}:{self.__port}/api/atelier/'
+        self.__DELETE_DOCS_URL: str = f'{protocol}://{self.__host}:{self.__port}{self.__api_base_url}'
         self.__DELETE_DOCS_URL += f'{self.__api_version}/{self.__namespace_iris}/docs'
 
-        self.__GET_DOC_URL: str = f'{protocol}://{self.__host}:{self.__port}/api/atelier/'
+        self.__GET_DOC_URL: str = f'{protocol}://{self.__host}:{self.__port}{self.__api_base_url}'
         self.__GET_DOC_URL += f'{self.__api_version}/{self.__namespace_iris}/doc/'
 
-        self.__COMPLIE_DOCS_URL: str = f'{protocol}://{self.__host}:{self.__port}/api/atelier/'
+        self.__COMPLIE_DOCS_URL: str = f'{protocol}://{self.__host}:{self.__port}{self.__api_base_url}'
         self.__COMPLIE_DOCS_URL += f'{self.__api_version}/{self.__namespace_iris}/action/compile'
 
         self.__iris_session = requests.Session()
         self.__iris_session.headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
         self.__iris_session.auth = HTTPBasicAuth(iris_usr, iris_pwd)
 
-    def compile_docs(self, doc_list: str, flags: str = "cukb")-> None:
+    def compile_docs(self, doc_list: str)-> None:
         '''
         Receive a list with one or more files to compile. If no flags is passed,
         use default flags: "cukb"
         '''
-        logging.info(f'COMPILLING DOCUMENTS: {doc_list}')
-        response = self.__iris_session.post(self.__COMPLIE_DOCS_URL, doc_list)
+        url = self.__COMPLIE_DOCS_URL + '?source=0&flags=' + self.__compilation_flags
+        response = self.__iris_session.post(url, doc_list)
+        iris_response: dict = json.loads(response.text)
         match response.status_code:
-            case 200:
-                logging.info(f'DOCUMENT COMPILED!')
-                logging.info(response.text)
-            case 201:
-                logging.info(f'DOCUMENT UPDATED!')
-                logging.info(response.text)
-            case 409:
-                logging.warning(f'DOCUMENT UPDATE WITH CONFLICT!')
-                logging.warning(response.text)
-            case 423:
-                logging.warning(f'DOCUMENT IS LOCKED AND CANNOT BE COMPILED!')
-                logging.warning(response.text)
-            # default error status
+            case 200 | 201:
+                logging.info('\n'.join(iris_response['console']))
+            case 409 | 403:
+                logging.warning('\n'.join(logging.info(iris_response['console'])))
             case _:
-                logging.error(f'COMPILATION ERROR!')
-                logging.error(response.text)
+                logging.error(iris_response['result']['status'])
+                self.__has_error = True
 
     def delete_docs(self, doc_list: str)-> None:
         '''
         Receive a list with one or more files to delete.
         '''
-        logging.info(f'DELETING DOCUMENTS: {doc_list}')
         response = self.__iris_session.delete(self.__DELETE_DOCS_URL, data=doc_list)
+        iris_response: dict = json.loads(response.text)
         match response.status_code:
             case 200:
-                logging.info(f'DOCUMENTS DELETED!')
-                logging.info(response.text)
-            # default error status
+                logging.info('\n'.join(iris_response['console']))
             case _:
-                logging.error(f'DELETION ERROR!')
-                logging.error(response.text)
+                logging.error(iris_response['result']['status'])
 
     def deploy_docs(self, changed_files: list)-> None:
         '''
@@ -93,14 +86,12 @@ class IrisDeployer(object):
             # from document name to avoid error in IRIS.
             file_name = source_file.replace(self.__source_path, '').replace('/', '.')
             document = self.get_doc(file_name)
-            if document is None:
-                continue
-            
             # if document exists set If-None-Match header to avoid conflict error
-            if document.ts != "":
-                self.__iris_session.headers.update({'If-None-Match' : document.ts})
+            if document is not None:                
+                documentTimestamp = document['result']['ts']
+                if documentTimestamp != "":
+                    self.__iris_session.headers.update({'If-None-Match' : documentTimestamp})
             
-            print(f'PROCESSING FILE {source_file}')
             with open(source_file, 'r') as reader:
                 source_document = {'enc': False, 'content' : []}
                 content = []
@@ -108,7 +99,6 @@ class IrisDeployer(object):
                 for line in reader:
                     content.append(line.rstrip())
 
-                # JSON document with source code
                 source_document['content'] = content
                 self.put_doc(source_document, file_name)
 
@@ -118,80 +108,80 @@ class IrisDeployer(object):
 
         self.compile_docs(files_to_compile)
 
-    def get_doc(self, file_name: str)-> dict:
+    def get_doc(self, file_name: str)-> None:
         '''
         Get document before send a update avoiding false conflict error.
         '''
-        logging.info(f'GETTING DOCUMENT: {url}')
         url: str = self.__GET_DOC_URL + file_name
         response = self.__iris_session.get(url)
-        
+        iris_response: dict = json.loads(response.text)
         match response.status_code:
             case 200 | 404:
-                return json.load(response.text)
+                pass
             case _:
-                logging.error(f'ERROR GETTING DOCUMENT!')
-                logging.error(response.text)
-        
-        return None
+                logging.error(iris_response['result']['status'])
+                self.__has_error = True
         
     def put_doc(self, source_document: str, file_name: str)-> None:
         '''
         Send document to IRIS
         '''
-        url: str = self.__PUT_DOC_URL + file_name
-        logging.info(f'DEPLOYING DOCUMENT: {url}')
-        response = self.__iris_session.put(url, data=json.dumps(source_document, ensure_ascii=True))
-
+        url: str = self.__PUT_DOC_URL + file_name        
+        response = self.__iris_session.put(url, data=json.dumps(source_document, ensure_ascii=True))        
+        iris_response: dict = json.loads(response.text)
         match response.status_code:
-            case 200:
-                logging.info(f'DOCUMENT CREATED!')
-                logging.info(response.text)
-            case 201:
-                logging.info(f'DOCUMENT UPDATED!')
-                logging.info(response.text)
-            case 409:
-                logging.warning(f'DOCUMENT WITH CONFLICTS. NOT UPDATED!')
-                logging.warning(response.text)
-            case 425:
-                logging.warning(f'DOCUMENT IS LOCKED AND CANNOT BE WRITTEN!')
-                logging.warning(response.text)
-            # default status error
+            case 200 | 201:
+                pass
+            case 409 | 425:
+                logging.warning(iris_response['console'])
             case _:
-                logging.error(f'DOCUMENT SENT WITH ERROR!')
-                logging.error(response.text)
-
-if __name__ == '__main__':
-    iris_deployer = IrisDeployer(os.environ['INPUT_HOST'],
-                                int(os.environ['INPUT_PORT']),
-                                os.environ['INPUT_NAMESPACE_IRIS'],
-                                int(os.environ['INPUT_HTTPS']),
-                                os.environ['INPUT_BASE_API_URL'],
-                                os.environ['INPUT_IRIS_USR'],
-                                os.environ['INPUT_IRIS_PWD'],
-                                os.environ['INPUT_VERSION_API'],
-                                os.environ['INPUT_COMPILATION_FLAGS'],
-                                os.environ['INPUT_SOURCE_PATH'])
+                logging.error(iris_response['result']['status'])
+                self.__has_error = True
     
-    changed_files = os.environ['INPUT_CHANGED_FILES'].split(',')
-    if changed_files.count > 0:
-        iris_deployer.deploy_docs()
+    def exit(self) -> None:
+        '''Existis according __has_error flag'''
+        if self.__has_error:
+            sys.exit(1)
+        
+        sys.exit(0)
+
+def in_debug_mode():
+    """
+    Checks if the Python program is running in debug mode.
+    Returns True if in debug mode, False otherwise.
+    """
+    return ((hasattr(sys, 'gettrace')) and (sys.gettrace() is not None))
+     
+if __name__ == '__main__':
+    if not in_debug_mode():
+        iris_deployer = IrisDeployer(os.environ['INPUT_HOST'],
+                                    int(os.environ['INPUT_PORT']),
+                                    os.environ['INPUT_NAMESPACE_IRIS'],
+                                    int(os.environ['INPUT_HTTPS']),
+                                    os.environ['INPUT_BASE_API_URL'],
+                                    os.environ['INPUT_IRIS_USR'],
+                                    os.environ['INPUT_IRIS_PWD'],
+                                    os.environ['INPUT_VERSION_API'],
+                                    os.environ['INPUT_COMPILATION_FLAGS'],
+                                    os.environ['INPUT_SOURCE_PATH'])
+        
+        changed_files = os.environ['INPUT_CHANGED_FILES'].split(',')
+        if changed_files.count > 0:
+            iris_deployer.deploy_docs()
+        else:
+            logging.info('0 FILES TO DEPLOY!')
+    
+        deleted_files = os.environ['INPUT_DELETED_FILES'].split(',')
+        if deleted_files.count > 0:
+            iris_deployer.delete_docs('["' + '","'.join(deleted_files.split(',')) \
+                .replace(os.environ['INPUT_SOURCE_PATH'], '').replace('/', '.') + '"]')
+        
+        iris_deployer.exit()        
     else:
-        logging.info('0 FILES TO DEPLOY!')
-
-    deleted_files = os.environ['INPUT_DELETED_FILES'].split(',')
-    if deleted_files.count > 0:
-        iris_deployer.delete_docs('["' + '","'.join(deleted_files.split(',')) \
-            .replace(os.environ['INPUT_SOURCE_PATH'], '').replace('/', '.') + '"]')
-
-#
-# Standalone test need to comment the __main__ block above and uncomment the block bellow #
-#
-'''
-source_path = 'C:\\Users\\Cristiano Silva\\OneDrive - CONFLUENCE\\Projetos\\Linker\\src\\'
-changed_files = ['C:\\Users\\Cristiano Silva\\OneDrive - CONFLUENCE\\Projetos\\Linker\\src\\test\\githubaction\\Test.cls', 'C:\\Users\\Cristiano Silva\\OneDrive - CONFLUENCE\\Projetos\\Linker\\src\\test\\githubaction\\Test1.cls', 'C:\\Users\\Cristiano Silva\\OneDrive - CONFLUENCE\\Projetos\\Linker\\src\\test\\githubaction\\Test2.cls']
-iris_deployer = IrisDeployer('192.168.1.7',57776,'LINKER_CORE_DEV', 0, '/api/atelier', 'cristiano.silva', 'Sup3rS3nh@', 'v2','cuk',source_path)
-iris_deployer.deploy_documents(changed_files)
-deleted_files = '["' + '","'.join(changed_files).replace(source_path, '').replace('\\', '.') + '"]'
-iris_deployer.delete_docs(deleted_files)
-'''
+        source_path = 'C:/Users/Cristiano Silva/OneDrive - CONFLUENCE/Projetos/Linker/src/'
+        changed_files = [f'{source_path}test/githubaction/Test1.cls', f'{source_path}test/githubaction/Teste3.cls', f'{source_path}test/githubaction/Test4.cls']
+        iris_deployer = IrisDeployer('189.1.174.141',57776,'LINKER_CORE_DEV', 0, '/api/atelier/', 'cristiano.silva', 'Sup3rS3nh@', 'v2','cuk',source_path)
+        iris_deployer.deploy_docs(changed_files)
+        deleted_files = '["' + '","'.join(changed_files).replace(source_path, '').replace('/', '.') + '"]'
+        iris_deployer.delete_docs(deleted_files)
+        iris_deployer.exit()
